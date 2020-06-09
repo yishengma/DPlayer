01b7266cc8bd52e4e9cddd441f2e3397817679db
mayisheng <13104862372@163.com>
2020-06-09 11:19:23 +0800

    调用ffmpeg方法成功

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,21 @@
 #include <jni.h>
 #include <string>
+extern "C" {
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+#include "libavutil/avutil.h"
+#include "libavfilter/avfilter.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
+
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_dplayer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    av_register_all();
+    const char * info = avcodec_configuration();
+    return env->NewStringUTF(info);
 }
